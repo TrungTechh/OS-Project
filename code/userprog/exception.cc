912856be676053a5045f4c1874ed98d8f3aa4f2a
Trung <deptraihaha123@gmail.com>
2022-04-02 10:04:33 +0700

    official project

---
+++ code/userprog/exception.cc
@@ -0,0 +1,325 @@
+// exception.cc 
+//	Entry point into the Nachos kernel from user programs.
+//	There are two kinds of things that can cause control to
+//	transfer back to here from user code:
+//
+//	syscall -- The user code explicitly requests to call a procedure
+//	in the Nachos kernel.  Right now, the only function we support is
+//	"Halt".
+//
+//	exceptions -- The user code does something that the CPU can't handle.
+//	For instance, accessing memory that doesn't exist, arithmetic errors,
+//	etc.  
+//
+//	Interrupts (which can also cause control to transfer from user
+//	code into the Nachos kernel) are handled elsewhere.
+//
+// For now, this only handles the Halt() system call.
+// Everything else core dumps.
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "main.h"
+#include "syscall.h"
+#include "ksyscall.h"
+
+#define MaxFileLength 32
+//----------------------------------------------------------------------
+// ExceptionHandler
+// 	Entry point into the Nachos kernel.  Called when a user program
+//	is executing, and either does a syscall, or generates an addressing
+//	or arithmetic exception.
+//
+// 	For system calls, the following is the calling convention:
+//
+// 	system call code -- r2
+//		arg1 -- r4
+//		arg2 -- r5
+//		arg3 -- r6
+//		arg4 -- r7
+//
+//	The result of the system call, if any, must be put back into r2. 
+//
+// If you are handling a system call, don't forget to increment the pc
+// before returning. (Or else you'll loop making the same system call forever!)
+//
+//	"which" is the kind of exception.  The list of possible exceptions 
+//	is in machine.h.
+//----------------------------------------------------------------------
+
+ char* stringUser2System(int addr, int convert_length = -1) {
+    int length = 0;
+    bool stop = false;
+    char* str;
+
+    do {
+        int oneChar;
+        kernel->machine->ReadMem(addr + length, 1, &oneChar);
+        length++;
+        // if convert_length == -1, we use '\0' to terminate the process
+        // otherwise, we use convert_length to terminate the process
+        stop = ((oneChar == '\0' && convert_length == -1) ||
+                length == convert_length);
+    } while (!stop);
+
+    str = new char[length];
+    for (int i = 0; i < length; i++) {
+        int oneChar;
+        kernel->machine->ReadMem(addr + i, 1,
+                                 &oneChar);  // copy characters to kernel space
+        str[i] = (unsigned char)oneChar;
+    }
+    return str;
+}
+
+void StringSys2User(char* str, int addr, int convert_length = -1) {
+    int length = (convert_length == -1 ? strlen(str) : convert_length);
+    for (int i = 0; i < length; i++) {
+        kernel->machine->WriteMem(addr + i, 1,
+                                  str[i]);  // copy characters to user space
+    }
+    kernel->machine->WriteMem(addr + length, 1, '\0');
+}
+
+void increaseProgramCounter(){
+	
+	
+	/* set previous programm counter (debugging only)*/
+	kernel->machine->WriteRegister(PrevPCReg, kernel->machine->ReadRegister(PCReg));
+
+	/* set programm counter to next instruction (all Instructions are 4 byte wide)*/
+	kernel->machine->WriteRegister(PCReg, kernel->machine->ReadRegister(PCReg) + 4);
+	
+	/* set next programm counter for brach execution */
+	kernel->machine->WriteRegister(NextPCReg, kernel->machine->ReadRegister(PCReg)+4);
+			
+}
+void handle_SC_ReadNum() {
+    int result = SysReadNum();
+	kernel->machine->WriteRegister(2, result);
+	return increaseProgramCounter();
+}
+
+void handle_SC_PrintNum() {
+    int character = kernel->machine->ReadRegister(4);
+	SysPrintNum(character);
+	return increaseProgramCounter();
+}
+
+void
+ExceptionHandler(ExceptionType which)
+{
+    int type = kernel->machine->ReadRegister(2);
+
+    //DEBUG(dbgSys, "Received Exception " << which << " type: " << type << "\n");
+
+    switch (which) {
+		case NoException:  // return control to kernel
+				kernel->interrupt->setStatus(SystemMode);
+				DEBUG(dbgSys, "Switch to system mode\n");
+				break;
+		case PageFaultException:
+		case ReadOnlyException:
+		case BusErrorException:
+		case AddressErrorException:
+		case OverflowException:
+		case IllegalInstrException:
+		case NumExceptionTypes:
+			cerr << "Error " << which << " occurs\n";
+			SysHalt();
+			ASSERTNOTREACHED();
+		
+		case SyscallException:
+			switch(type) {
+				case SC_Halt:
+					DEBUG('a', "\nShutdown, initiated by user program. ");
+					printf("\nShutdown, initiated by user program. ");
+					SysHalt();
+					ASSERTNOTREACHED();
+					return;
+
+				case SC_Add:
+				{
+					DEBUG(dbgSys, "Add " << kernel->machine->ReadRegister(4) << " + " << kernel->machine->ReadRegister(5) << "\n");
+
+					/* Process SysAdd Systemcall*/
+					int result;
+					result = SysAdd(/* int op1 */(int)kernel->machine->ReadRegister(4),
+							/* int op2 */(int)kernel->machine->ReadRegister(5));
+
+					DEBUG(dbgSys, "Add returning with " << result << "\n");
+					/* Prepare Result */
+					kernel->machine->WriteRegister(2, (int)result);
+					return ;
+				}
+
+				case SC_ReadNum:
+				{
+					int result = SysReadNum();
+					kernel->machine->WriteRegister(2, result);
+					return increaseProgramCounter();
+				}
+					
+                case SC_PrintNum:
+				{
+					int character = kernel->machine->ReadRegister(4);
+                    SysPrintNum(character);
+                    return increaseProgramCounter();
+				}
+					
+				case SC_ReadChar:{
+					char res = SysReadChar();
+					kernel->machine->WriteRegister(2, (int)res);
+					return increaseProgramCounter();	
+				}
+
+				case SC_PrintChar:
+				{
+					char character = (char)kernel->machine->ReadRegister(4);
+					SysPrintChar(character);
+					return increaseProgramCounter();
+				}
+					
+				case SC_RandomNum:
+				{
+					int result = SysRandomNum();
+					kernel->machine->WriteRegister(2, (int)result);
+					DEBUG(dbgSys, "Random number is " << result << "\n");
+					return increaseProgramCounter();
+				}
+
+				case SC_ReadString:
+                {
+					int addr = kernel->machine->ReadRegister(4);
+                    int length = kernel->machine->ReadRegister(5);
+
+                    if (length > 10) {
+                        DEBUG(dbgSys, "String's length exceeds max length!");
+                        SysHalt();
+                    }
+
+                    char* buffer = stringUser2System(addr);
+					buffer=SysReadString(length);
+                    StringSys2User(buffer, addr, length);
+                    delete[] buffer;
+                    buffer = NULL;
+                    return increaseProgramCounter();
+				}
+	
+				case SC_PrintString:
+				{
+					int memPtr = kernel->machine->ReadRegister(4);  // read address of C-string
+					char* buffer = stringUser2System(memPtr);
+					SysPrintString(buffer, strlen(buffer));
+					delete[] buffer;
+					return increaseProgramCounter();
+				}
+
+				case SC_CreateFile:
+                {
+					int virtAddr = kernel->machine->ReadRegister(4);
+                    char* fileName = stringUser2System(virtAddr);
+
+                    if (SysCreateFile(fileName))
+                        kernel->machine->WriteRegister(2, 0);
+                    else
+                        kernel->machine->WriteRegister(2, -1);
+
+                    delete[] fileName;
+                    return increaseProgramCounter();
+				}
+				
+				//Open for write != open for read
+				case SC_Open:
+				{
+					int virtAddr;
+					char* fileName;
+
+					virtAddr = kernel->machine->ReadRegister(4);
+					DEBUG(dbgSys, "\nReading filename.");
+
+					fileName = stringUser2System(virtAddr);
+					kernel->machine->WriteRegister(2, SysOpen(fileName));
+
+					return increaseProgramCounter();
+				}
+
+				case SC_Close:
+				{
+					DEBUG(dbgSys, "Reading fileID.");
+					int id = kernel->machine->ReadRegister(4);
+					kernel->machine->WriteRegister(2, SysClose(id));
+
+					return increaseProgramCounter();
+				}
+
+				case SC_Remove: {
+                    int virtAddr;
+                    char* filename;
+
+                    virtAddr = kernel->machine->ReadRegister(4);
+
+                    DEBUG(dbgSys, "Reading filename.");
+                    filename = stringUser2System(virtAddr);
+
+                    kernel->machine->WriteRegister(2, SysRemove(filename));
+
+                    delete filename;
+                    return increaseProgramCounter();
+                }
+
+				case SC_Read:
+				{
+					int virtAddr = kernel->machine->ReadRegister(4);
+					int charCount = kernel->machine->ReadRegister(5);
+					int fileId = kernel->machine->ReadRegister(6);
+					
+					char* buffer = stringUser2System(virtAddr, charCount);
+					DEBUG(dbgFile,"Read " << charCount << " chars from file " << fileId << "\n");
+
+					kernel->machine->WriteRegister(2, SysRead(buffer, charCount, fileId));
+					StringSys2User(buffer, virtAddr, charCount);
+
+    				delete[] buffer;
+                    return increaseProgramCounter();
+				}
+                case SC_Write:
+                {
+					int virtAddr = kernel->machine->ReadRegister(4);
+					int charCount = kernel->machine->ReadRegister(5);
+					int fileId = kernel->machine->ReadRegister(6);
+					char* buffer = stringUser2System(virtAddr, charCount);
+
+					DEBUG(dbgFile,"Write " << charCount << " chars to file " << fileId << "\n");
+
+					kernel->machine->WriteRegister(2, SysWrite(buffer, charCount, fileId));
+					StringSys2User(buffer, virtAddr, charCount);
+
+					delete[] buffer;
+                    return increaseProgramCounter();
+				}
+				case SC_Seek:
+                {
+					int seekPos = kernel->machine->ReadRegister(4);
+					int fileId = kernel->machine->ReadRegister(5);
+
+					kernel->machine->WriteRegister(2, SysSeek(seekPos, fileId));
+
+					return increaseProgramCounter();
+				}
+
+				default:
+					cerr << "Unexpected system call " << type << "\n";
+					break;
+			}
+			break;
+		default:
+			cerr << "Unexpected user mode exception" << (int)which << "\n";
+			break;
+    }
+    ASSERTNOTREACHED();
+
+}
---
+++ code/userprog/ksyscall.h
@@ -0,0 +1,314 @@
+/**************************************************************
+ *
+ * userprog/ksyscall.h
+ *
+ * Kernel interface for systemcalls 
+ *
+ * by Marcus Voelp  (c) Universitaet Karlsruhe
+ *
+ **************************************************************/
+
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
+
+#include "kernel.h"
+#include "synchconsole.h"
+#include <stdlib.h> 
+
+#define STRINGLENGTH 255
+#define ENDSTRING '\0'
+
+#define LF ((char)10)
+#define CR ((char)13)
+#define TAB ((char)9)
+#define SPACE ((char)' ')
+
+/* A buffer to read and write number */
+#define MAX_NUM_LENGTH 11
+char _numberBuffer[MAX_NUM_LENGTH + 2];
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+char isBlank(char c) { return c == 10 || c == 11 || c == 13 || c == 9 || c == ' '; }
+
+void readUntilBlank() {
+    memset(_numberBuffer, 0, sizeof(_numberBuffer));
+    char c = kernel->synchConsoleIn->GetChar();
+
+    if (c == EOF) {
+        DEBUG(dbgSys, "Unexpected end of file - number expected");
+        return;
+    }
+
+    if (isBlank(c)) {
+        DEBUG(dbgSys, "Unexpected white-space - number expected");
+        return;
+    }
+
+    int n = 0;
+    while (!(isBlank(c) || c == EOF || c == '\n')) {
+      _numberBuffer[n++] = c;
+      if (n > MAX_NUM_LENGTH) {
+          DEBUG(dbgSys, "Number is too long");
+          return;
+      }
+      c = kernel->synchConsoleIn->GetChar();
+  }
+}
+
+bool compareNumAndString(int integer, const char *s) {
+    if (integer == 0) return strcmp(s, "0") == 0;
+
+    int len = strlen(s);
+
+    if (integer < 0 && s[0] != '-') return false;
+
+    if (integer < 0) s++, --len, integer = -integer;
+
+    while (integer > 0) {
+        int digit = integer % 10;
+
+        if (s[len - 1] - '0' != digit) return false;
+
+        --len;
+        integer /= 10;
+    }
+
+    return len == 0;
+}
+
+int SysReadNum() {
+    readUntilBlank();
+    
+    int len = strlen(_numberBuffer);
+    // Read nothing -> return 0
+    if (len == 0) return 0;
+
+    // Check comment below to understand this line of code
+    if (strcmp(_numberBuffer, "-2147483648") == 0) return -2147483648LL;
+
+    bool nega = (_numberBuffer[0] == '-');
+    int zeros = 0;
+    bool is_leading = true;
+    int num = 0;
+    for (int i = nega; i < len; ++i) {
+        char c = _numberBuffer[i];
+        if (c == '0' && is_leading)
+            ++zeros;
+        else
+            is_leading = false;
+        if (c < '0' || c > '9') {
+            DEBUG(dbgSys, "Expected number but " << _numberBuffer << " found");
+            return 0;
+        }
+        num = num * 10 + (c - '0');
+    }
+    // 00            01 or -0
+    if (zeros > 1 || (zeros && (num || nega))) {
+        DEBUG(dbgSys, "Expected number but " << _numberBuffer << " found");
+        return 0;
+    }
+
+    if (nega)
+        num = -num;
+
+    if (len <= MAX_NUM_LENGTH - 2) return num;
+
+    if (compareNumAndString(num, _numberBuffer))
+        return num;
+    else
+        DEBUG(dbgSys,
+              "Expected int32 number but " << _numberBuffer << " found");
+
+    return num;
+}
+
+void SysPrintNum(int num) {
+    if (num == 0) return kernel->synchConsoleOut->PutChar('0');
+
+    if (num == -2147483648LL) {
+        kernel->synchConsoleOut->PutChar('-');
+        for (int i = 0; i < 10; ++i)
+            kernel->synchConsoleOut->PutChar("2147483648"[i]);
+        return;
+    }
+
+    if (num < 0) {
+        kernel->synchConsoleOut->PutChar('-');
+        num = -num;
+    }
+    int n = 0;
+    while (num) {
+        _numberBuffer[n++] = num % 10;
+        num /= 10;
+    }
+    for (int i = n - 1; i >= 0; --i)
+        kernel->synchConsoleOut->PutChar(_numberBuffer[i] + '0');
+}
+
+char SysReadChar() { return kernel->synchConsoleIn->GetChar(); }
+
+void SysPrintChar(char character) {
+    kernel->synchConsoleOut->PutChar(character);
+}
+
+int SysRandomNum() { 
+  int random;
+
+	/* initialize random seed: */
+	srand(time(NULL));
+
+	/* generate secret number between 1 and 10: */
+	random = rand() % 2147483647 + 1;
+	return random;
+}
+
+char* SysReadString(int length) {
+    char tmp;
+    char* buffer = new char[length + 1];
+    for (int i = 0; i < length + 1; i++) {
+        tmp = SysReadChar();
+        if (i < length) buffer[i] = tmp;
+    }
+    
+    buffer[length] = ENDSTRING;
+
+    return buffer;
+}
+
+void SysPrintString(char* buffer, int length) {
+    for (int i = 0; i < length; i++) {
+        kernel->synchConsoleOut->PutChar(buffer[i]);
+    }
+}
+
+bool SysCreateFile(char* fileName){
+    bool success;
+    int fileNameLength = strlen(fileName);
+
+    if (fileNameLength == 0) {
+        DEBUG(dbgSys, "\nFile name can't be empty");
+        success = false;
+
+    } else if (fileName == NULL) {
+        DEBUG(dbgSys, "\nNot enough memory in system");
+        success = false;
+
+    } else {
+        DEBUG(dbgSys, "\nFile's name read successfully");
+        if (!kernel->fileSystem->Create(fileName,0)) {
+            DEBUG(dbgSys, "\nError creating file");
+            success = false;
+        } else {
+            success = true;
+        }
+    }
+
+    return success;
+}
+
+
+int SysOpen(char* fileName)
+{
+    int fileNameLength = strlen(fileName);
+
+    if (fileNameLength == 0) {
+        DEBUG(dbgSys, "\nFile name can't be empty");
+        return -1;
+    }
+
+    else if (fileName == NULL) {
+        DEBUG(dbgSys, "\nNot enough memory in system");
+        return -1;
+    }
+
+    else {
+        int openFileID = kernel->fileSystem->FindFreeSlot();
+        DEBUG(dbgSys, "\nFile's name read successfully");
+        if (openFileID == -1) {
+            DEBUG(dbgSys, "\nThere are fullfill processes!");
+            return -1;
+        }
+
+        else if ((kernel->fileSystem->openFile[openFileID] =
+                      kernel->fileSystem->Open(fileName)) != NULL) {
+            
+            kernel->fileSystem->openFileName[openFileID] = (fileName);
+            DEBUG(dbgSys, "\nOpen file successfully");
+
+            return openFileID;
+        }
+
+        else {
+            DEBUG(dbgSys, "\nError opening file!");
+            return -1;
+        }
+    }
+}
+
+int SysClose(int id) { return kernel->fileSystem->Close(id); }
+
+int SysRemove(char* fileName) {
+    int fileNameLength = strlen(fileName);
+
+    if (fileNameLength == 0) {
+        DEBUG(dbgSys, "\nFile name can't be empty");
+        return -1;
+    }
+
+    else if (fileName == NULL) {
+        DEBUG(dbgSys, "\nNot enough memory in system");
+        return -1;
+
+    }
+
+    else {
+        if (kernel->fileSystem->isOpen(fileName)) {
+            DEBUG(dbgSys, "\nFile " << fileName << " is openning!");
+            return -1;
+        } else {
+            bool success = kernel->fileSystem->Remove(fileName);
+            if (!success) {
+                DEBUG(dbgSys, "\nError removing file!");
+                return -1;
+            } else {
+                DEBUG(dbgSys, "\nRemoving file successfully!");
+                return 0;
+            }
+        }
+    }
+}
+
+int SysRead(char *buffer, int size, int id)
+{
+    if (id == 0) {
+        return kernel->synchConsoleIn->GetString(buffer, size);
+    }
+    return kernel->fileSystem->Read(buffer, size, id);
+}
+
+int SysWrite(char *buffer, int size, int id)
+{
+    if (id == 1) {
+        return kernel->synchConsoleOut->PutString(buffer, size);
+    }
+    return kernel->fileSystem->Write(buffer, size, id);
+}
+
+int SysSeek(int seekPos, int fileId) {
+    if (fileId <= 1) {
+        DEBUG(dbgSys, "\nCan't seek in console");
+        return -1;
+    }
+    return kernel->fileSystem->Seek(seekPos, fileId);
+}
+
+#endif /* ! __USERPROG_KSYSCALL_H__ */
